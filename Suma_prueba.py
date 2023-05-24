3ceeb0e42fae2ac940fcf51609801075bb14d6bd
mmi1006 <80346364+mmi1006@users.noreply.github.com>
2023-05-25 00:43:34 +0200

    Add files via upload

--- Suma_prueba.py
+++ Suma_prueba.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[1]:
 
 
-def sumar (x, y):
-    resultado = x+y
-    return resultado
+#!pip install streamlit
+
+
+# In[2]:
+
+
+
+
+
+# In[4]:
+
+
+import streamlit as st
+import Suma_prueba
+
+def main():
+    st.title("Interfaz de Mi Función Específica")
+
+    # Entrada de datos
+    input1 = st.number_input("Ingrese el primer número:")
+    input2 = st.number_input("Ingrese el segundo número:")
+
+    # Botón para ejecutar la función
+    if st.button("Ejecutar"):
+        # Llama a la función y obtén el resultado
+        resultado = Suma_prueba(input1, input2)
+
+        # Muestra el resultado
+        st.write("El resultado es:", resultado)
+
+if __name__ == "__main__":
+    main()
 
